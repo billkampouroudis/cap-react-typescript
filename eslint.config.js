b42f2a002f6799989b75b9019f007861c216c01d
Vasilis Kampouroudis <v.kampouroudis@gmail.com>
2024-11-22 23:01:34 +0200

    Create basic file structure and add eslint, prettier and husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,29 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  pluginJs.configs.recommended,
+  tseslint.configs.base,
+  pluginReact.configs.flat.recommended,
+  prettierRecommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+    plugins: {},
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'prettier/prettier': 'warn',
+      semi: ['warn', 'always'],
+      'react/react-in-jsx-scope': 'off',
+      'max-len': ['error', { code: 120 }],
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect React version
+      },
     },
   },
-)
+];
